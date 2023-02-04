cfdec2ddb761bdd14f7b810508fbf9f16a2a5b81
Felipe Santos <felipecassiors@gmail.com>
2023-02-04 14:47:42 -0300

    feat: set correct latest release

---
+++ scripts/set_latest_release.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Fail on error
+set -o errexit
+# Fail on pipeline
+set -o pipefail
+# Disable undefined variable reference
+set -o nounset
+
+latest_tag=$(git tag -l --sort=-version:refname | head -1)
